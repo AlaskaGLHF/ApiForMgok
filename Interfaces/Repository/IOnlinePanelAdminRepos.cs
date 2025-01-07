a27f6b88c73118165ad2f7035c0e8d7ac5d7790b
WolfisDragon <mesenko.dima@yandex.ru>
2025-01-07 20:30:42 +0300

    Add OnlinePanelAdminService but need refactoring

--- Interfaces/Repository/IOnlinePanelAdminRepos.cs
+++ Interfaces/Repository/IOnlinePanelAdminRepos.cs
@@ -14,6 +14,7 @@ namespace ApiForMgok.Interfaces.Repository
         Task<Employee> UpdateEmployeeAsync(Employee employee);
         Task<Employee> CreateEmployeeAsync(Employee newEmployee);
         Task<List<Address>> GetAllAddressesAsync();
+        Task<Address> GetAddressByIdAsync(int id);
         Task<Address> CreateAddressAsync(Address newaddress);
         Task<Address> UpdateAddressAsync(Address address);
 
